53b4ec33f695c54b9fa38ca93d6a48401bf2b272
NadhifAdyatmaZ <nadhifadyat@gmail.com>
2023-04-13 09:42:25 +0700

    update filter & chart graphic

--- lib/pages/monitoring/produksi_energi_screen.dart
+++ lib/pages/monitoring/produksi_energi_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:manajemen_aset/pages/monitoring/produksi_energi_chart.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+import 'sales_data.dart';
 
 class ProduksiEnergiScreen extends StatefulWidget {
   final String idCluster;
@@ -11,11 +14,47 @@ class ProduksiEnergiScreen extends StatefulWidget {
 }
 
 class _ProduksiEnergiScreenState extends State<ProduksiEnergiScreen> {
+  final data = [
+    new SalesData(0, 1500000),
+    new SalesData(1, 1735000),
+    new SalesData(2, 1678000),
+    new SalesData(3, 1890000),
+    new SalesData(4, 1907000),
+    new SalesData(5, 2300000),
+    new SalesData(6, 2360000),
+    new SalesData(7, 1980000),
+    new SalesData(8, 2654000),
+    new SalesData(9, 2789070),
+    new SalesData(10, 3020000),
+    new SalesData(11, 3245900),
+    new SalesData(12, 4098500),
+    new SalesData(13, 4500000),
+    new SalesData(14, 4456500),
+    new SalesData(15, 3900500),
+    new SalesData(16, 5123400),
+    new SalesData(17, 5589000),
+    new SalesData(18, 5940000),
+    new SalesData(19, 6367000),
+  ];
+
+  _getSeriesData() {
+    List<charts.Series<SalesData, int>> series = [
+      charts.Series(
+          id: "Sales",
+          data: data,
+          domainFn: (SalesData series, _) => series.year,
+          measureFn: (SalesData series, _) => series.sales,
+          colorFn: (SalesData series, _) =>
+              charts.MaterialPalette.blue.shadeDefault)
+    ];
+    return series;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color.fromARGB(0, 255, 255, 255),
+        backgroundColor: const Color.fromARGB(0, 255, 255, 255),
         elevation: 0,
         title: const Text(
           'Produksi Energi (kWh) - Tuban',
@@ -23,11 +62,86 @@ class _ProduksiEnergiScreenState extends State<ProduksiEnergiScreen> {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ProduksiEnergiChart(
-          idCluster: widget.idCluster,
-        ),
-      ),
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(
+                          color: Colors.greenAccent,
+                        ),
+                        borderRadius:
+                            BorderRadius.circular(20.0), //<-- SEE HERE
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        child: const Text(
+                          'Harian',
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(
+                          color: Colors.greenAccent,
+                        ),
+                        borderRadius:
+                            BorderRadius.circular(20.0), //<-- SEE HERE
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        child: const Text(
+                          'Bulanan',
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    Card(
+                      shape: RoundedRectangleBorder(
+                        side: const BorderSide(
+                          color: Colors.greenAccent,
+                        ),
+                        borderRadius:
+                            BorderRadius.circular(20.0), //<-- SEE HERE
+                      ),
+                      child: Container(
+                        padding: const EdgeInsets.all(10),
+                        child: const Text(
+                          'Tahunan',
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ProduksiEnergiChart(
+                idCluster: widget.idCluster,
+              ),
+              Expanded(
+                child: charts.LineChart(
+                  _getSeriesData(),
+                  animate: true,
+                ),
+              )
+            ],
+          )),
     );
   }
 }
