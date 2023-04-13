a3f11e3ea9ee65d7cbd47ac963467777d61a9ec5
NadhifAdyatmaZ <nadhifadyat@gmail.com>
2023-04-13 11:42:39 +0700

    update icon & screenshot

--- lib/pages/monitoring/produksi_energi_screen.dart
+++ lib/pages/monitoring/produksi_energi_screen.dart
@@ -15,26 +15,26 @@ class ProduksiEnergiScreen extends StatefulWidget {
 
 class _ProduksiEnergiScreenState extends State<ProduksiEnergiScreen> {
   final data = [
-    new SalesData(0, 1500000),
-    new SalesData(1, 1735000),
-    new SalesData(2, 1678000),
-    new SalesData(3, 1890000),
-    new SalesData(4, 1907000),
-    new SalesData(5, 2300000),
-    new SalesData(6, 2360000),
-    new SalesData(7, 1980000),
-    new SalesData(8, 2654000),
-    new SalesData(9, 2789070),
-    new SalesData(10, 3020000),
-    new SalesData(11, 3245900),
-    new SalesData(12, 4098500),
-    new SalesData(13, 4500000),
-    new SalesData(14, 4456500),
-    new SalesData(15, 3900500),
-    new SalesData(16, 5123400),
-    new SalesData(17, 5589000),
-    new SalesData(18, 5940000),
-    new SalesData(19, 6367000),
+    SalesData(0, 1500000),
+    SalesData(1, 1735000),
+    SalesData(2, 1678000),
+    SalesData(3, 1890000),
+    SalesData(4, 1907000),
+    SalesData(5, 2300000),
+    SalesData(6, 2360000),
+    SalesData(7, 1980000),
+    SalesData(8, 2654000),
+    SalesData(9, 2789070),
+    SalesData(10, 3020000),
+    SalesData(11, 3245900),
+    SalesData(12, 4098500),
+    SalesData(13, 4500000),
+    SalesData(14, 4456500),
+    SalesData(15, 3900500),
+    SalesData(16, 5123400),
+    SalesData(17, 5589000),
+    SalesData(18, 5940000),
+    SalesData(19, 6367000),
   ];
 
   _getSeriesData() {
@@ -139,52 +139,44 @@ class _ProduksiEnergiScreenState extends State<ProduksiEnergiScreen> {
               ),
               Row(
                 children: [
-                  Expanded(child: Icon(Icons.insert_chart)),
+                  const Expanded(child: Icon(Icons.show_chart)),
                   Expanded(
                     flex: 2,
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("PLTB"),
-                        ],
-                      ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Text("PLTB"),
+                      ],
                     ),
                   ),
-                  Expanded(child: Icon(Icons.insert_chart)),
+                  const Expanded(child: Icon(Icons.insights)),
                   Expanded(
                     flex: 2,
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("PLTS"),
-                        ],
-                      ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Text("PLTS"),
+                      ],
                     ),
                   ),
-                  Expanded(child: Icon(Icons.insert_chart)),
+                  const Expanded(child: Icon(Icons.stacked_line_chart)),
                   Expanded(
                     flex: 2,
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Diesel"),
-                        ],
-                      ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Text("Diesel"),
+                      ],
                     ),
                   ),
-                  Expanded(child: Icon(Icons.insert_chart)),
+                  const Expanded(child: Icon(Icons.legend_toggle)),
                   Expanded(
                     flex: 2,
-                    child: Container(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text("Load"),
-                        ],
-                      ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const <Widget>[
+                        Text("Load"),
+                      ],
                     ),
                   )
                 ],
